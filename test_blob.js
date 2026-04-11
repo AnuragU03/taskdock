a6a74d2b3687eb330ecf5b1a53be62173c762912
AnuragU03 <anuragugargol@gmail.com>
2026-04-11 16:52:39 +0530

    chore: clean up temporary test files

--- test_blob.js
+++
@@ -1,9 +0,0 @@
-const { BlobServiceClient } = require('@azure/storage-blob');
-try {
-  // Simulate string containing literal double quotes that Azure ENV variables sometimes inject
-  const connectionString = '"DefaultEndpointsProtocol=https;EndpointSuffix=core.windows.net;AccountName=test;AccountKey=123;BlobEndpoint=https://test.blob.core.windows.net/"';
-  BlobServiceClient.fromConnectionString(connectionString);
-  console.log('Success!');
-} catch (e) {
-  console.error('ERROR:', e.message, e.code);
-}
--- test_blob_empty.js
+++
@@ -1,7 +0,0 @@
-const { BlobServiceClient } = require('@azure/storage-blob');
-try {
-  BlobServiceClient.fromConnectionString('');
-  console.log('Success!');
-} catch (e) {
-  console.error('ERROR:', e.message, e.code);
-}
